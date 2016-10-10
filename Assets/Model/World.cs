675b22ee72de91a79b69de4f580a74d8193e590a
Scott Price <scottprice7@gmail.com>
2016-10-10 15:33:58 -0700

    first push

---
+++ Assets/Model/World.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World
+{
+
+    Tile[,] tiles;
+    int width;
+    int height;
+
+    public World(int width = 100, int height = 100)
+    {
+        this.width = width;
+        this.height = height;
+
+        tiles = new Tile[width,height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tiles[x, y] = new Tile(this, x, y);
+            }
+        }
+
+        Debug.Log("World created: " +width+height+" titles");
+    }
+
+    public Tile GetTileAt(int x, int y)
+    {
+        if (x > width || x < 0 || y > height || y <0)
+        {
+            Debug.LogError("Tile x: "+x+" y: "+y);
+            return null;
+        }
+        return tiles[x, y];
+    }
+}
